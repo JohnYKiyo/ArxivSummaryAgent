3dc3861c78fabbf7e16178835dbde9779fdb36b8
Yu Kiyokawa <y-kiyokawa@bit-quark.com>
2025-07-15 15:41:44 +0900

    arXiv論文処理エージェントの実装と関連ツールの追加

--- arxiv/tools/__init__.py
+++ arxiv/tools/__init__.py
@@ -1,5 +1,22 @@
 """Arxiv tools module for paper fetching and processing."""
 
+from .arxiv_agent_tools import (
+    arxiv_file_lister_tool,
+    arxiv_file_reader_tool,
+    arxiv_tex_expander_tool,
+    expand_tex_file,
+    get_all_files,
+    read_file_content,
+)
 from .arxiv_tools import arxiv_eprint_fetcher_tool, fetch_eprint_from_arxiv
 
-__all__ = ["fetch_eprint_from_arxiv", "arxiv_eprint_fetcher_tool"]
+__all__ = [
+    "fetch_eprint_from_arxiv",
+    "arxiv_eprint_fetcher_tool",
+    "get_all_files",
+    "read_file_content",
+    "expand_tex_file",
+    "arxiv_file_lister_tool",
+    "arxiv_file_reader_tool",
+    "arxiv_tex_expander_tool",
+]
